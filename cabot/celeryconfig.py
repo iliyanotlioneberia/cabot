7a9b0ffe09c6cd7d477df10b7424d3822cef9e68
iliyanotlioneberia <iliyanmk@gmail.com>
2016-02-09 14:27:33 +0000

    stopped tasks

--- cabot/celeryconfig.py
+++ cabot/celeryconfig.py
@@ -15,24 +15,24 @@ CELERY_REDIS_MAX_CONNECTIONS = 1000
 # CELERY_TASK_RESULT_EXPIRES = 10
 # CELERY_IGNORE_RESULT = True
 
-CELERYBEAT_SCHEDULE = {
-    'run-all-checks': {
-        'task': 'cabot.cabotapp.tasks.run_all_checks',
-        'schedule': timedelta(seconds=60),
-    },
-    'update-shifts': {
-        'task': 'cabot.cabotapp.tasks.update_shifts',
-        'schedule': timedelta(seconds=1800),
-    },
-    'clean-db': {
-        'task': 'cabot.cabotapp.tasks.clean_db',
-        'schedule': timedelta(seconds=60*60*24),
-    },
-    ### deletes 'celery' key in redis every 6 hours
-    'delete_celery_cache_redis': {
-        'task': 'cabot.cabotapp.tasks.delete_celery_cache_redis',
-        'schedule': timedelta(seconds=60*60*1),
-    },
-}
+# CELERYBEAT_SCHEDULE = {
+#     'run-all-checks': {
+#         'task': 'cabot.cabotapp.tasks.run_all_checks',
+#         'schedule': timedelta(seconds=60),
+#     },
+#     'update-shifts': {
+#         'task': 'cabot.cabotapp.tasks.update_shifts',
+#         'schedule': timedelta(seconds=1800),
+#     },
+#     'clean-db': {
+#         'task': 'cabot.cabotapp.tasks.clean_db',
+#         'schedule': timedelta(seconds=60*60*24),
+#     },
+#     ### deletes 'celery' key in redis every 6 hours
+#     'delete_celery_cache_redis': {
+#         'task': 'cabot.cabotapp.tasks.delete_celery_cache_redis',
+#         'schedule': timedelta(seconds=60*60*1),
+#     },
+# }
 
 CELERY_TIMEZONE = 'UTC'
